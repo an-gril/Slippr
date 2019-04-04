63b1bf4d1f019c1fabf3fdc52453f5c64041bcac
an-gril <acgrillet@gmail.com>
2019-04-04 18:22:06 +0100

    Release related changes and fixes.

---
+++ Default/sub_menu.php
@@ -0,0 +1,15 @@
+<!-- Slippr is (C) 2019, Andrew Grillet, Released under GPL2.0 -->
+
+<?php
+
+$menu = array(
+"Home" => "<td/>Home screen",
+"Design" => "<td/>Design rules for Slippr",
+"Deploy" => "<td/>Typical deployment script",
+"Release" => "<td/>Release notes",
+"Help" => "<td/>Get some help!"
+);
+
+make_page_menu($menu, "images/RTFM.jpg");
+
+?>
