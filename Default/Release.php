1f74bb2faf2217ad92b2d0098935e26c4dcd8cac
an-gril <acgrillet@gmail.com>
2019-04-17 20:37:22 +0100

    release info updated to reflet that it is version 1.0 as there is now a version 2.

--- Default/Release.php
+++ Default/Release.php
@@ -1,14 +1,14 @@
 <!-- Slippr - a Bootstrap based Framework -->
 <!-- Slippr is (C) 2019, Andrew Grillet, Released under GPL2.0 -->
 
-<h2 align="center">Slippr - first ever release.</h2>
+<h2 align="center">Slippr - first release.</h2>
 <p> &nbsp;</p>
 <p align=center><img src="images/Area.png"/> </p>
 <p> &nbsp;</p>
 <p>This version of Slipper is the first release. At this time, it has had 
 some testing, and should not explode without warning.
 </p>
-<p>It was built on 4 April 2019.
+<p>It was last modified on 17 April 2019.
 </p> 
 <p>At that time, all features actually implemented were believed to work 
 as advertised.
