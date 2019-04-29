18e5a7673ac83ee3f662b6c975aebfde8293474b
an-gril <acgrillet@gmail.com>
2019-04-29 21:08:21 +0100

    The Google css and js should be in local/title.php, and not in index.php, as they are normally customised.

--- lib/local/title.php
+++ lib/local/title.php
@@ -6,7 +6,7 @@
 <title>Slippr</title>
 
 
-<!-- These scripts are used by reCaptcha, whihc is used on the contact form.  
+<!-- These scripts are used by reCaptcha, which is used on the contact form.  
 You can delete them in your copy if you will not be needing a reCaptcha. 
 -->
 <script src='https://www.google.com/recaptcha/api.js' async defer'>
@@ -17,3 +17,39 @@ You can delete them in your copy if you will not be needing a reCaptcha.
 		document.getElementById("Contact").submit();
 	}
 </script>
+
+ <!-- Get this stuff from CDN and google cos its faster than hosting myself, 
+and should be cached from other websites anyway! 
+External CSS should be loaded before External Javascript - to allow parallelism
+-->
+     
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- My own bootstrap theme CSS code - Needs to be on the target website -->
+<link rel="stylesheet" href="/css/bootstrap-extras.css">
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" async></script> 
+
+<!-- jQuery library (used by Modals) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" async></script>
+
+<link rel="apple-touch-icon" sizes="57x57" href="images/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="images/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="images/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="images/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="images/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="images/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="images/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="images/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="images/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="images/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="images/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
+<link rel="manifest" href="/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="images/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+
